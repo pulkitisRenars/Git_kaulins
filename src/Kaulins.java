b7e5bace5520163f6dae47f239a2514be308221e
Pulkis <renars.pulkis@gmail.com>
2022-01-18 10:45:11 +0200

    Papildinats ar ciklu

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,14 +1,16 @@
 import java.util.Random;
 
 public class Kaulins {
-static void mest(){
+static void mest(int m){
 	Random rand = new Random();
 int uzmestkaulins;
+for(int i = 0;i<m;i++){
 uzmestkaulins = rand.nextInt(6)+1;
 System.out.println("Uzkrita : "+uzmestkaulins);
 }
+}
 	public static void main(String[] args) {
-mest();
+mest(3);
 		
 		
 		
