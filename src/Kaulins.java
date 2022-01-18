3c1eb4f7dfac4dab4a2073ea24cf0a4b7f471123
Pulkis <renars.pulkis@gmail.com>
2022-01-18 10:37:25 +0200

    Ievietota metode

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,12 +1,14 @@
 import java.util.Random;
 
 public class Kaulins {
-
-	public static void main(String[] args) {
-Random rand = new Random();
+static void mest(){
+	Random rand = new Random();
 int uzmestkaulins;
 uzmestkaulins = rand.nextInt(6)+1;
 System.out.println("Uzkrita : "+uzmestkaulins);
+}
+	public static void main(String[] args) {
+mest();
 		
 		
 		
