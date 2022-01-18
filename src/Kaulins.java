00e05c5eb44cdcfff307bc10f3649b330ef2f98e
Pulkis <renars.pulkis@gmail.com>
2022-01-18 11:08:50 +0200

    uzlabots

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,13 +1,19 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Kaulins {
 
 	public static void main(String[] args) {
 Random rand = new Random();
+Scanner dati = new Scanner(System.in);
+System.out.println("Cik reizes uzmest kaulinu?");
+int x = dati.nextInt();
 int uzmestkaulins;
+for(int i =0; i<x;i++){
 uzmestkaulins = rand.nextInt(6)+1;
 System.out.println("Uzkrita : "+uzmestkaulins);
-		
+}
+dati.close();
 		
 		
 		
