a102a4466fe71c4ab30dc19593d0fbfa16646fbe
Pulkis <renars.pulkis@gmail.com>
2022-02-01 10:42:16 +0200

    Paplasinata programma ar lielakā sk piefiksesanu

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -7,10 +7,15 @@ public class Kaulins {
 static void mest(int m){
 	Random rand = new Random();
 int uzmestkaulins;
+int max=0;
 for(int i = 0;i<m;i++){
 uzmestkaulins = rand.nextInt(6)+1;
+if(max<uzmestkaulins){
+	max=uzmestkaulins;
+}
 System.out.println("Uzkrita : "+uzmestkaulins);
 }
+System.out.println("No "+m+" metieniem lielakais bija: "+max);
 }
 	public static void main(String[] args) {
 		Scanner dati = new Scanner(System.in);
